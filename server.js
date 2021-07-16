488703c40ca6776241585c6dd557d0559ce2fda3
Michael Callahan <mcallahan0@yahoo.com>
2021-07-16 14:41:00 -0400

    Added server.js file that sets up the server through Express and uses path to route the html files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+  });
+
+  // If no matching route is found default to home
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
