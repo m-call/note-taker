a33017c46c324f6e0555850f5a0c848561383e84
Michael Callahan <mcallahan0@yahoo.com>
2021-07-17 00:26:14 -0400

    Set the route for the db.json file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// Importing necessary frameworks and libraries
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
+// Setting up express as an app variable and setting the port for the server
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,17 +12,18 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
-
+// Setting the HTML routes for the server
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
+});
+
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'))
+});
 
   // If no matching route is found default to home
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+});
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
