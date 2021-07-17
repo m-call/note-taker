b792d264f538170b9803c32ac735406cb4b73f05
Michael Callahan <mcallahan0@yahoo.com>
2021-07-17 13:09:28 -0400

    Fixed post request so the new note immediately appears on the left side of the screen and is saved to the db.json file using the fs.writeFile method

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,23 @@
 // Importing the necessary database file, Node file system module, and NPM package for the id
 const fs = require('fs');
 const db = require('../db/db.json');
-const id = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = (app) => {
 
     // Gets the JSON data from the db 
-    app.get('/api/notes', (req, res) => res.json(db));
+    app.get('/api/notes', (req, res) => {
+        res.json(db);
+    });
 
     // Posts JSON data to the db
     app.post('/api/notes', (req, res) => {
         db.push(req.body);
+        let notes = JSON.stringify(db);
+        fs.writeFile(__dirname + '/../db/db.json', notes, (err) => {
+            if (err) throw err;
+        })
+        res.end();
     });
 
     app.delete('/api/notes/:id'), (req, res) => {
