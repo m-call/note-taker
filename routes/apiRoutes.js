dda63a6112d3481fca69cc6a0e1a1b9d225c5222
Michael Callahan <mcallahan0@yahoo.com>
2021-07-17 13:57:10 -0400

    Added functionality to the adding of a new note so that it properly adds to the db.json and immediately appears on the left as well as persists when the server is stopped and restarted. Tried to implement the functionality for deleting a note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,22 +6,38 @@ const { v4: uuidv4 } = require('uuid');
 module.exports = (app) => {
 
     // Gets the JSON data from the db 
-    app.get('/api/notes', (req, res) => {
-        res.json(db);
-    });
+    app.get('/api/notes', (req, res) => res.json(db));
 
     // Posts JSON data to the db
     app.post('/api/notes', (req, res) => {
-        db.push(req.body);
+
+        // Creates a new note in object form assigning an id, title, and text
+        let newNote = {
+            id: uuidv4(),
+            title: req.body.title,
+            text: req.body.text
+        };
+
+        // Adds the newly created note object to the db.json file
+        db.push(newNote);
+
+        // Turns the JSON data from the db.json file into a string so it can be used in the fs.writeFile method
         let notes = JSON.stringify(db);
+
+        // Overwrites the db.json file with the newly added note(s)
         fs.writeFile(__dirname + '/../db/db.json', notes, (err) => {
             if (err) throw err;
         })
         res.end();
+
     });
 
     app.delete('/api/notes/:id'), (req, res) => {
-
+        for (i = 0; i < db.length; i++) {
+            if (db[i].id == req.params.id) {
+                console.log('test');
+            }
+        }
     }
 
 };
